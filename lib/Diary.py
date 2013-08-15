bc6e97a8a0d99fa7753840af5e2786284738d9f6
Jacek Szpot <maligree@gmail.com>
2013-08-15 13:01:34 +0200

    Mocked writing values into Diary.

--- lib/Diary.py
+++ lib/Diary.py
@@ -1,10 +1,13 @@
 class Diary(object):
 
     def __init__(self):
-        pass
+        self.load_record = []
 
     def start(self):
         pass
 
     def stop(self):
         print "[!] Closing the diary."
+
+    def write_load(self, load_report):
+        self.load_record.append(load_report)
--- lib/Foreman.py
+++ lib/Foreman.py
@@ -1,10 +1,33 @@
+import time
+from multiprocessing import Process
+from probes.LoadAvg import LoadAvg
+
 class Foreman(object):
     
     def __init__(self, diary):
         self.diary = diary
+        # Hardcoding some things for now.
+        self.probes = {
+            'LoadAvg': LoadAvg()        
+        }
+
+        self.worker_ps = None
 
     def start(self):
-        pass
+        self.worker_ps = Process(target=self.run)
+        self.worker_ps.start()
 
     def stop(self):
         print "[!] Telling the foreman to take a break.."
+        self.worker_ps.terminate()
+
+    def run(self):
+        while True:
+            self.probe_system()
+            time.sleep(2)
+
+    def probe_system(self):
+        probe_time = int(time.time())
+        print "[*] Probing system, probe time: %s" % (time.ctime(probe_time))
+        
+        self.diary.write_load(self.probes['LoadAvg'].report())

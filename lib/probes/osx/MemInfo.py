2f643a13f11bc0ce249c0e3e36087c26c9c0ba43
Jacek Szpot <maligree@gmail.com>
2013-08-23 21:22:12 +0200

    Changed type of memory reported; added area chart which is fucking sweet.

--- lib/probes/osx/MemInfo.py
+++ lib/probes/osx/MemInfo.py
@@ -9,10 +9,16 @@ class MemInfo:
         phys_mem = phys_mem.strip().split() 
         free = int(phys_mem[9][:-1])
         used = int(phys_mem[7][:-1])
+        active = int(phys_mem[3][:-1])
+        inactive = int(phys_mem[5][:-1])
+        wired = int(phys_mem[1][:-1])
         total = free + used
 
         return { 
                 "free": free,
                 "used": used,
+                "active": active,
+                "inactive": inactive,
+                "wired": wired,
                 "total": total,
         }

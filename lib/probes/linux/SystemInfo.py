2a28c488791416af7941fa06d77c4585a5b47ab1
jnowak <kubuus@gmail.com>
2013-08-31 00:39:45 +0200

    SystemInfo Linux probe serving uptime, OS name, and hostname added

---
+++ lib/probes/linux/SystemInfo.py
@@ -0,0 +1,16 @@
+import commands
+
+class SystemInfo(object):
+    def __init__(self):
+        self.hostname = commands.getoutput("hostname")
+        self.os = commands.getoutput("uname -a")
+    
+    def report(self):
+        f = open("/proc/uptime","r")
+        sec = int(float(f.readline().split()[0]))
+
+        return {
+                "uptime": sec,
+                "hostname": self.hostname,
+                "os": self.os,
+                }

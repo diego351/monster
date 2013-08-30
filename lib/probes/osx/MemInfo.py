1556a588ef0a6e781cf4770471b7d0e8cf71c98b
Jakub Nowak <jnowak@Jakubs-Mac.local>
2013-08-30 21:30:04 +0200

    Ultimate speed up of osx MemProbe

--- lib/probes/osx/MemInfo.py
+++ lib/probes/osx/MemInfo.py
@@ -1,7 +1,9 @@
-import subprocess
+#import subprocess
+import commands
+import math
 
 class MemInfo:
-    def report(self):
+    def reportOld(self):
         top_ps = subprocess.Popen(['top', '-l', '1'], stdout=subprocess.PIPE)
         phys_mem = subprocess.check_output(('grep', 'PhysMem'), stdin=top_ps.stdout)
         top_ps.wait()
@@ -21,4 +23,30 @@ class MemInfo:
                 "inactive": inactive,
                 "wired": wired,
                 "total": total,
-        }
+                }
+
+    def report(self):
+        output = commands.getoutput("vm_stat")
+        splitted = output.split("\n")
+        pageSize = int(splitted[0].split()[7])
+        free = int(splitted[1].split()[2][:-1])
+        active = int(splitted[2].split()[2][:-1])
+        inactive = int(splitted[3].split()[2][:-1])
+        speculative = int(splitted[4].split()[2][:-1])
+        wired = int(splitted[5].split()[3][:-1])
+
+        pageSizeShift = int(math.sqrt(pageSize >> 10))
+        free += speculative
+        total = active + inactive + wired + free
+
+        # as power!
+
+        return {
+                "free": free >> (10 - pageSizeShift) ,
+                "total": total >> (10 - pageSizeShift),
+                "active": active >> (10 - pageSizeShift),
+                "inactive": inactive >> (10 - pageSizeShift),
+                "wired": wired >> (10 - pageSizeShift),
+                }
+        
+        

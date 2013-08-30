092bff25728f9babfcf28d08aa4fc88354634253
Jakub Nowak <jnowak@Jakubs-Mac.local>
2013-08-31 00:56:39 +0200

    SystemInfo probe OSX support added

---
+++ lib/probes/osx/SystemInfo.py
@@ -0,0 +1,16 @@
+import commands
+
+class SystemInfo(object):
+    def __init__(self):
+        self.hostname = commands.getoutput("hostname")
+        self.os = commands.getoutput("uname -a")
+        self.boottime = int(commands.getoutput("sysctl kern.boottime").split()[4][:-1])
+    def report(self):
+
+        currentSec = int(commands.getoutput("date +%s"))
+
+        return {
+                "uptime": currentSec - self.boottime,
+                "hostname": self.hostname,
+                "os": self.os,
+                }

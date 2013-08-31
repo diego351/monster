9cf411f5976cf76d3c21fd286793cff787d32201
diego351 <kubuus@gmail.com>
2013-08-31 12:18:55 +0200

    postgres probe fully working

--- lib/probes/db/Postgres.py
+++ lib/probes/db/Postgres.py
@@ -1,24 +1,44 @@
 import psycopg2
+import psycopg2.extras
+
 
 class Postgres(object):
     def __init__(self,username,password,db):
         self.dbName = db
         self.connString = "host='localhost' dbname='%s' user='%s' password='%s'" %(self.dbName,username,password)
         self.conn = psycopg2.connect(self.connString)
-        self.cursor = self.conn.cursor()
+        self.cursor =self.conn.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
+        self.firstTime = True
+        self.emptyDict = {
+                            "returned": 0,
+                            "fetched": 0,
+                            "inserted": 0,
+                            "updated": 0,
+                            "deleted": 0,
+                            }
+
     def getCurrentData(self):
         select = "SELECT * FROM pg_stat_database WHERE datname = '%s'" % self.dbName
         self.cursor.execute(select)
-        temp =  dict(self.cursor.fetchone())
-        
+        temp = self.cursor.fetchone()
         return {
-                "returned": temp["tup_returned"],
-                "fetched": temp["tup_fetched"],
-                "inserted": temp["tup_inserted"],
-                "updated": temp["tup_updated"],
-                "deleted": temp["tup_deleted"],
+                "returned": int(str(temp["tup_returned"])),
+                "fetched":  int(str(temp["tup_fetched"])),
+                "inserted": int(str(temp["tup_inserted"])),
+                "updated":  int(str(temp["tup_updated"])),
+                "deleted": int(str(temp["tup_deleted"])),
                 }
-
     def report(self):
-        pass
+        if self.firstTime:
+            self.myBuffer = self.getCurrentData()
+            self.firstTime = False
+            return self.emptyDict
+        else:
+            curr = self.getCurrentData()
+            toRet = self.subtractDictValues(curr,self.myBuffer)
+            self.myBuffer = curr
+            return toRet
         
+    
+    def subtractDictValues(self,a,b):
+        return dict( [ (n, a.get(n, 0) - b.get(n, 0)) for n in set(a)|set(b) ] ) #thank you stackoverflow!

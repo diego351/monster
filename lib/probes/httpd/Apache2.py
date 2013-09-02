15bf580e51aa43f0ecec2fc79bf079596183419c
jnowak <kubuus@gmail.com>
2013-09-02 17:56:18 +0200

    apache probe patch

--- lib/probes/httpd/Apache2.py
+++ lib/probes/httpd/Apache2.py
@@ -20,56 +20,62 @@ class Apache2(object):
                 if os.path.exists(path):
                     print "Found an Apache2 log file in %s" % (path,) 
                     self.logPath = path
+        self.lastSize = 0
+        self.retNone = {"transfer": 0,
+                        "requests": 0,
+                        }
 
     def report(self, interval=5):
         delta = timedelta(seconds = interval)
-        log = open(self.logPath)
+        currSize = os.path.getsize(self.logPath)
+        if self.lastSize != currSize:
+            self.lastSize = currSize
+            log = open(self.logPath)
 
-        currentDateTime = datetime.now()
-        limiter = currentDateTime - delta
+            currentDateTime = datetime.now()
+            limiter = currentDateTime - delta
 
-        retNone = {"transfer": 0,
-                    "requests": 0,
+            while True:
+                line = log.readline()[:-1]
+                #print "#" + line + "#"
+                if line == "":
+                    #print "just oldies here."
+                    return self.retNone
+                logLineDate = line.split('"')[0].replace("]","[").split("[")[1] # [*]
+                logDateTime = datetime.strptime(logLineDate[0:-6], "%d/%b/%Y:%H:%M:%S") # [*] [*]
+                if logDateTime < limiter:
+                    continue
+                else:
+                    log.seek(-1*len(line)+1,1) # omitted one important line!
+                    break
+
+
+            requests = 0
+            transfer = 0
+        
+            for line in log:
+                requests += 1
+                splitted = line.split('"')
+                print splitted
+                ipDateMesh = splitted[0]
+                getLinkMesh = splitted[1]
+                code, size = splitted[2].split()
+                if size != "-":
+                    transfer += int(size)
+                #fromLink = splitted[3] # it's optional!
+                #splitted[4] # is always blank
+                #browser = splitted[5]
+                ipDateMesh = ipDateMesh.replace("]","[")
+                asdf = ipDateMesh.split("[")
+                date = asdf[1]
+                ip,meta0,meta1 = asdf[0].split()
+                header, link, protocol = splitted[1].split()
+                code, size = splitted[2].split()
+                # sure, we have a lot of info, lets leave them for next features. 
+        
+            log.close()
+            return {"transfer":transfer,
+                    "requests":requests,
                     }
-        while True:
-            line = log.readline()[:-1]
-            #print "#" + line + "#"
-            if line == "":
-                #print "just oldies here."
-                return retNone
-            logLineDate = line.split('"')[0].replace("]","[").split("[")[1] # [*]
-            logDateTime = datetime.strptime(logLineDate[0:-6], "%d/%b/%Y:%H:%M:%S") # [*] [*]
-            if logDateTime < limiter:
-                continue
-            else:
-                log.seek(-1*len(line)+1,1) # omitted one important line!
-                break
-
-
-        requests = 0
-        transfer = 0
-        
-        for line in log:
-            requests += 1
-            splitted = line.split('"')
-            print splitted
-            ipDateMesh = splitted[0]
-            getLinkMesh = splitted[1]
-            code, size = splitted[2].split()
-            if size != "-":
-                transfer += int(size)
-            #fromLink = splitted[3] # it's optional!
-            #splitted[4] # is always blank
-            #browser = splitted[5]
-            ipDateMesh = ipDateMesh.replace("]","[")
-            asdf = ipDateMesh.split("[")
-            date = asdf[1]
-            ip,meta0,meta1 = asdf[0].split()
-            header, link, protocol = splitted[1].split()
-            code, size = splitted[2].split()
-            # sure, we have a lot of info, lets leave them for next features. 
-        
-        log.close()
-        return {"transfer":transfer,
-                "requests":requests,
-                }
+        else:
+            return self.retNone

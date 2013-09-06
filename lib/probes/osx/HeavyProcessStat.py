380661d543aae454ea1df5432107076deea11d6e
student2 <student2@dyn-41-208.ict.pwr.wroc.pl>
2013-09-06 10:08:36 +0200

    Heavy process patch applied, Fuck! its working!

--- lib/probes/osx/HeavyProcessStat.py
+++ lib/probes/osx/HeavyProcessStat.py
@@ -14,11 +14,11 @@ class HeavyProcessStat(object):
         proc = {}
         out = commands.getoutput("ps xrco %cpu,command") # -o for custom format, -r for cpu sorting, -c for just process name (not full path, -x for all processes)
         for line in out.split("\n")[1:]: #ommiting shit
-            s = line.split(" ", 3) # btw how to not pass a separator (use default separator == any whitespace) and split with max split? Any ideas? "\s" separator doesn't seem to work
-            if proc.has_key(s[3]): # this is nessesary since we can have processes with not unique name like "Google Chrome"
-                proc[s[3]] += float(s[2])
+            s = line.split(None, 1) # btw how to not pass a separator (use default separator == any whitespace) and split with max split? Any ideas? "\s" separator doesn't seem to work
+            if proc.has_key(s[1]): # this is nessesary since we can have processes with not unique name like "Google Chrome"
+                proc[s[1]] += float(s[0])
             else:
-                proc[s[3]] = float(s[2])
+                proc[s[1]] = float(s[0])
                 howMany -= 1
 
             if howMany == 0: # means we already gathered top 5 proceses with unique name!

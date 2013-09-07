17c2058fb7dc92abc5e57ea1b9de20a0c61cfc83
Jacek Szpot <maligree@gmail.com>
2013-09-07 22:55:10 +0200

    Okay that wasn't fun.

--- assets/static/js/regular/nginx-geo-ip-chart.js
+++ assets/static/js/regular/nginx-geo-ip-chart.js
@@ -1,23 +1,28 @@
 function drawNginxMap() {
   $.ajax('/api/nginx_geocache', { dataType: 'json' }).done(function(response) {
     var ip_table = [];   
-    ip_table[0] = ['Latitude', 'Longitude', 'Requests'];
+    /* an empty geochart complains about needing only two columns */
     var ip_list = response.nginx_ips.ips;
-    for (var i = 0; i < ip_list.length; i++) {
-      var ip_entry = [
-        ip_list[i].latitude,
-        ip_list[i].longitude,
-        ip_list[i].number
-      ];
-      ip_table.push(ip_entry);
+    if (ip_list.length == 0) {
+        ip_table[0] = ['Latitude', 'Longitude'];
+    } else {
+        ip_table[0] = ['Latitude', 'Longitude', 'Requests'];
+        for (var i = 0; i < ip_list.length; i++) {
+          var ip_entry = [
+            ip_list[i].latitude,
+            ip_list[i].longitude,
+            ip_list[i].number
+          ];
+          ip_table.push(ip_entry);
+        }
+        console.log(ip_table);
     }
-    console.log(ip_table);
 
     var options = {
       legend: 'top',
       fontName: 'maven pro',
       displayMode: 'markers',
-//      colors: ['#ff2d55', '#5ac8fa', '#4cd964']
+      colors: ['#ff2d55', '#5ac8fa', '#4cd964']
     };
     var chart_data = google.visualization.arrayToDataTable(ip_table);
     var chart = new google.visualization.GeoChart(document.getElementById('nginx-geo-ip-div'));

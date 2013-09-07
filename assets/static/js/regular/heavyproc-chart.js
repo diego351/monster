898c4c33191c706174dae8d7ba1e2f8f25752399
Jacek Szpot <maligree@gmail.com>
2013-09-07 15:43:28 +0200

    Basic version of chart-drawing javascript madness.

---
+++ assets/static/js/regular/heavyproc-chart.js
@@ -0,0 +1,64 @@
+function drawHeavyProcessTables() {
+  /* CPU table first. */ 
+  $.ajax('/api/heavy_process_stat', { dataType: 'json' }).done(function(response) {
+    var cpu_table = [];
+    cpu_table[0] = ['Process', 'CPU usage', 'Change'];
+    for (var i = 0; i < response.cpuList.length; i++) {
+        var cpu_entry = [
+            response.cpuList[i].process_name,
+            response.cpuList[i].cpu_usage,
+            response.cpuList[i].change
+        ];  
+        cpu_table.push(cpu_entry);
+    }
+
+    /* mem */
+    var mem_table = [];
+    mem_table[0] = ['Process', 'Memory usage', 'Change'];
+    for (var i = 0; i < response.memList.length; i++) {
+        var mem_entry = [
+            response.memList[i].process_name,
+            response.memList[i].mem_usage,
+            response.memList[i].change
+        ];
+    };
+
+    var options = {
+      curveType: 'function',
+      legend: 'top',
+      fontName: 'maven pro',
+      vAxis: {
+        maxValue: 3.0,
+        minValue: 0.0,
+        viewWindow: {
+          min: 0.0
+        },
+        gridlines: {
+          count: 16,
+          color: '#f6f6f6'
+        }
+      },
+      hAxis: {
+        gridlines: {
+          color: '#f6f6f6'
+        },
+        viewWindow: {
+          min:0,
+          max:29},
+        },
+        colors: ['#ff2d55', '#5ac8fa', '#4cd964']
+    };
+    var cpu_chart_data = google.visualization.arrayToDataTable(cpu_table);
+    var cpu_chart = new google.visualization.Table(document.getElementById('hps-cpu-chart-div'));
+    cpu_chart.draw(cpu_chart_data, options);
+
+    var mem_char_data = google.visualization.arrayToDataTable(mem_table);
+    var mem_chart = new google.visualization.Table(document.getElementById('hps-mem-chart-div'));
+    mem_chart.draw(mem_chart_data, options);
+
+    if (!hps_refresh) {
+      hps_refresh = setInterval(drawHeavyProcessTables, 2000);
+    }
+    
+  });
+};

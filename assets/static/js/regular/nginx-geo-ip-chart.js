6a3d7941df34bacb0f4e09c31715b8ce8362507d
Jacek Szpot <maligree@gmail.com>
2013-09-07 22:20:05 +0200

    Let's see.

---
+++ assets/static/js/regular/nginx-geo-ip-chart.js
@@ -0,0 +1,31 @@
+function drawNginxMap() {
+  $.ajax('/api/nginx_geocache', { dataType: 'json' }).done(function(response) {
+    var ip_table = [];   
+    ip_table[0] = ['Latitude', 'Longitude', 'IP', 'Requests'];
+    var ip_list = response.nginx_ips.ips;
+    for (var i = 0; i < ip_list.length; i++) {
+      var ip_entry = [
+        ip_list[i].latitude,
+        ip_list[i].longitude,
+        ip_list[i].ip,
+        ip_list[i].number
+      ];
+      ip_table.push(ip_entry);
+    }
+
+    var options = {
+      legend: 'top',
+      fontName: 'maven pro',
+      displayMode: 'markers',
+      colors: ['#ff2d55', '#5ac8fa', '#4cd964']
+    };
+    var chart_data = google.visualization.arrayToDataTable(ip_table);
+    var chart = new google.visualization.GeoChart(document.getElementById('nginx-geo-ip-div'));
+    chart.draw(chart_data, options);
+
+    if (!nginx_map_refresh) {
+      nginx_map_refresh = setInterval(drawNginxMap, 2000);
+    }
+    
+  });
+};

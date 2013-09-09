b3385bdd4adaddbf8865cebeedd4f4537e89960b
student2 <student2@dyn-41-208.ict.pwr.wroc.pl>
2013-09-09 12:26:28 +0200

    now it returns "boottime" property

--- lib/probes/osx/SystemInfo.py
+++ lib/probes/osx/SystemInfo.py
@@ -1,16 +1,17 @@
 import commands
 
 class SystemInfo(object):
-    def __init__(self):
+    def __init__(self,options):
         self.hostname = commands.getoutput("hostname")
         self.os = commands.getoutput("uname -a")
         self.boottime = int(commands.getoutput("sysctl kern.boottime").split()[4][:-1])
-    def report(self,options):
+    def report(self):
 
         currentSec = int(commands.getoutput("date +%s"))
 
         return {
                 "uptime": currentSec - self.boottime,
+                "boottime": self.boottime,
                 "hostname": self.hostname,
                 "os": self.os,
                 }

2f16b0792729115bdd26906dc4b20666a4c8c1de
Jacek Szpot <maligree@gmail.com>
2013-09-10 10:18:30 +0200

    Rewrote some more.

--- assets/static/js/regular/mysql-chart.js
+++ assets/static/js/regular/mysql-chart.js
@@ -1,38 +1,45 @@
-function drawMysqlColumns() {
-  $.ajax('/api/mysql', { dataType: 'json'}).done(function(response) {
-    var mysqlTable = [];
-    mysqlTable[0] = ['Index', 'Selects', 'Inserts', 'Updates', 'Deletes', 'Connections'];
-    for (var i = 0; i < response.mysql_stats.length; i++) {
-      mysqlEntry = [
-        i,
-        response.mysql_stats[i]['select'],
-        response.mysql_stats[i]['insert'],
-        response.mysql_stats[i]['update'],
-        response.mysql_stats[i]['delete'],
-        response.mysql_stats[i]['connections']
-      ];
-      mysqlTable.push(mysqlEntry);
-    }
-    var mysqlData = new google.visualization.arrayToDataTable(mysqlTable);
-    var options = { 
-      colors: ['#ff2d55', '#5ac8fa', '#4cd964', '#ffcc00'],
-      fontName: 'maven pro',
-      viewWindow: {
-        min: 0.0
-      },
-      hAxis: {
-        gridlines: {
-          color: '#f6f6f6'
-        }
-      },
-      vAxis: {
-        gridlines:{color: '#f6f6f6'}}
-    };
-    var chart = new google.visualization.ColumnChart(document.getElementById('mysql_columns'));
-    chart.draw(mysqlData, options);
+probe.register({
+  init: function() {
+    this.mysql_chart_obj = new google.visualization.ColumnChart(document.getElementById('mysql_columns'));
+  },
+  paint: function() {
+    var mysql_chart_obj = this.mysql_chart_obj;
+    $.ajax('/api/mysql', { dataType: 'json'}).done(function(response) {
+      var mysqlTable = [];
+      mysqlTable[0] = ['Index', 'Selects', 'Inserts', 'Updates', 'Deletes', 'Connections'];
+      for (var i = 0; i < response.mysql_stats.length; i++) {
+        mysqlEntry = [
+          i,
+          response.mysql_stats[i]['select'],
+          response.mysql_stats[i]['insert'],
+          response.mysql_stats[i]['update'],
+          response.mysql_stats[i]['delete'],
+          response.mysql_stats[i]['connections']
+        ];
+        mysqlTable.push(mysqlEntry);
+      }
+      var mysqlData = new google.visualization.arrayToDataTable(mysqlTable);
+      var options = { 
+        colors: ['#ff2d55', '#5ac8fa', '#4cd964', '#ffcc00'],
+        fontName: 'maven pro',
+        viewWindow: {
+          min: 0.0
+        },
+        hAxis: {
+          gridlines: {
+            color: '#f6f6f6'
+          }
+        },
+        vAxis: {
+          gridlines:{color: '#f6f6f6'}}
+      };
+      
+      mysql_chart_obj.draw(mysqlData, options);
 
-    if (!mysql_refresh) {
-      mysql_refresh = setInterval(drawMysqlColumns, 3000);
-    }
-  });        
+    });
+  }
+});
+
+function drawMysqlColumns() {
+          
 }
--- assets/static/js/regular/nginx-geo-ip-chart.js
+++ assets/static/js/regular/nginx-geo-ip-chart.js
@@ -1,53 +1,59 @@
-function drawNginxMap() {
-  $.ajax('/api/nginx_geocache', { dataType: 'json' }).done(function(response) {
-    var ip_table = [];   
-    /* an empty geochart complains about needing only two columns */
-    var ip_list = response.nginx_ips.ips;
-    if (ip_list.length == 0) {
-        ip_table[0] = ['Latitude', 'Longitude'];
-    } else {
-        ip_table[0] = ['Latitude', 'Longitude', 'Requests'];
-        for (var i = 0; i < ip_list.length; i++) {
-          var ip_entry = [
-            ip_list[i].latitude,
-            ip_list[i].longitude,
-            ip_list[i].number
-          ];
-          ip_table.push(ip_entry);
-        }
-        console.log(ip_table);
-    }
+probes.register({
+  init: function() {
+    this.map_1_obj = new google.visualization.GeoChart(document.getElementById('nginx-geo-ip-div-1'));
+    this.map_2_obj = new google.visualization.GeoChart(document.getElementById('nginx-geo-ip-div-2'));
+  },
+  paint: function() {
+    var map_1_obj = this.map_1_obj;
+    var map_2_obj = this.map_2_obj;
+    $.ajax('/api/nginx_geocache', { dataType: 'json' }).done(function(response) {
+      var ip_table = [];   
+      /* an empty geochart complains about needing only two columns */
+      var ip_list = response.nginx_ips.ips;
+      if (ip_list.length == 0) {
+          ip_table[0] = ['Latitude', 'Longitude'];
+      } else {
+          ip_table[0] = ['Latitude', 'Longitude', 'Requests'];
+          for (var i = 0; i < ip_list.length; i++) {
+            var ip_entry = [
+              ip_list[i].latitude,
+              ip_list[i].longitude,
+              ip_list[i].number
+            ];
+            ip_table.push(ip_entry);
+          }
+          console.log(ip_table);
+      }
 
-    var options = {
-      legend: 'top',
-      fontName: 'maven pro',
-      displayMode: 'markers',
-      colors: ['#ff2d55', '#5ac8fa', '#4cd964']
-    };
-    var chart_data = google.visualization.arrayToDataTable(ip_table);
+      var options = {
+        legend: 'top',
+        fontName: 'maven pro',
+        displayMode: 'markers',
+        colors: ['#ff2d55', '#5ac8fa', '#4cd964']
+      };
+      var chart_data = google.visualization.arrayToDataTable(ip_table);
 
-    if (document.getElementById('nginx-geo-ip-div-1').style.display == "") {
-        var paint_on = 'nginx-geo-ip-div-2';
-        var old_chart = 'nginx-geo-ip-div-1';
-    } else {
-        var paint_on = 'nginx-geo-ip-div-1';
-        var old_chart = 'nginx-geo-ip-div-2';
-    }
-    console.log("Painting on " + paint_on);
-
-    var chart = new google.visualization.GeoChart(document.getElementById(paint_on));
-    chart.draw(chart_data, options);
-    /* I didn't check if charts exposed some onFinishedPainting hook,
-     * so I'm doing the stupid thing - giving it 1.5 seconds to paint.
-     */
-    setTimeout(function() {
-        document.getElementById(old_chart).style.display = "none";
-        document.getElementById(paint_on).style.display = "";
-    }, 1500);
-
-    if (!nginx_map_refresh) {
-      nginx_map_refresh = setInterval(drawNginxMap, 2000);
-    }
-    
-  });
-};
+      if (document.getElementById('nginx-geo-ip-div-1').style.display == "") {
+          var paint_on = 'nginx-geo-ip-div-2';
+          var old_chart = 'nginx-geo-ip-div-1';
+      } else {
+          var paint_on = 'nginx-geo-ip-div-1';
+          var old_chart = 'nginx-geo-ip-div-2';
+      }
+     
+      if (document.getElementById('nginx-geo-ip-div-1').style.display == "") {
+        map_2_obj.draw(chart_data, options);
+        setTimeout(function() {
+            document.getElementById('nginx-geo-ip-div-1').style.display = "none";
+            document.getElementById('nginx-geo-ip-div-2').style.display = "";
+        }, 1200);
+      } else {
+        map_1_obj.draw(chart_data, options);
+        setTimeout(function() {
+            document.getElementById('nginx-geo-ip-div-2').style.display = "none";
+            document.getElementById('nginx-geo-ip-div-1').style.display = "";
+        }, 1200);
+      }
+    });
+  }
+});
